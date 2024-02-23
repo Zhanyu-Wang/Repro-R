ac2344ef11bcf58ada5b6cbb464ff4590bebaa3e
xinlongdu <xinlongdu@icloud.com>
2024-02-22 21:38:29 -0500

    vectorized the T_stat function and the counting function to improve efficiency; rewrote accept function using the p_value function

--- p_val.R
+++ p_val.R
@@ -5,55 +5,73 @@ library(ddalpha)
 
 
 
-# the Mahalanobis depth 
-# x: a vector of dimension d, data: an (R+1) by d matrix containing (R+1) vectors
+# the Mahalanobis depth (modified to also take in a parameter theta)
+# x: each row of x is a vector whose distance we want to compute, 
+# data: an (R+1) by d matrix containing (R+1) vectors
 ma_depth <- function(x, data, theta){
   return(depth.Mahalanobis(x, data))
 }
 
+# test case for ma_depth
+data_test <- matrix(data = rnorm(50),
+                 nrow = 10,
+                 ncol =5)
+x_test <- data_test[1:2,]
+ma_depth(x_test, data_test, 0)
+
 
 
 # p_value function 
-p_value <- function(range, t_init, alpha, R, seeds, G, s_obs, T_stat = ma_depth){
-  s_sim <- function(t, i){
-    return(G(seeds[i, ], t))
-  }
+p_value <- function(lower_bd, upper_bd, t_init, seeds, G, s_obs, T_stat = ma_depth){
+  # extract the number of seeds R
+  dimensions <- dim(seeds)
+  R <- dimensions[1]
+  d <- dimensions[2]
   
-  # function that computes the simulated s values
-  data <- function(t){
-    data_array <- array(s_obs, c(1, length(s)))
-    for(i in seq(R)){
-      data_array <- rbind(data_array, s_sim(t, i))
+  # a function that generate R simulated values using the seeds and G, store s_obs and s_sim in an R+1 by d matrix
+  s <- function(theta){
+    s_values <- matrix(s_obs, 
+                       nrow = R+1, 
+                       ncol = d, 
+                       byrow = TRUE)
+    for(i in 1:R){
+      s_values[i,] <- G(seeds[i-1,], theta)
     }
   }
   
-  count <- function(t){
-    s_values <- data(t)
-    t_obs <- T_stat(s_obs, s_values, t)
-    max <- 1 + t_obs
-    for(i in seq(R)){
-      t_i <- T_stat(s_values[1 + i, ], s_values, t)
-      if(t_i <= t_obs){
-        max <- max + 1
-      }
-    }
-    return(-max)
+  # function that computes and stores the simulated statistics using T_stat
+  statistics <- function(theta){
+    t_vec <- T_stat(s(theta), s(theta), theta)
   }
   
+  # define the counting function that we feed into optim
+  count <- function(theta){
+    s_values <- s(theta)
+    t_values <- statistics(s_values, s_values, theta)
+    ct <- sum(t_values[-1] <= t_values[1]) + t_values[1]
+    return(-ct)
+  }
+  
+  # finding the largest 
   max <- -optim(par = t_init, 
                 fn = count,
-                lower = range[,1],
-                uppper = range[,2])$value
+                lower = lower_bd,
+                uppper = upper_bd)$value
   
-  if(max >= floor(alpha*(R+1)) + 1){
-    return(TRUE)
-  }else{
-    return(FALSE)
-  }
+  # compute the p value and return
+  p_val <- 1/(R+1) * min(floor(max)+1, R+1)
+  return(p_val)
 }
 
 
 
+# accept function
+accept <- function(alpha, lower_bd, upper_bd, t_init, seeds, G, s_obs, T_stat = ma_depth){
+  p_val <- p_value(lower_bd, upper_bd, t_init, seeds, G, s_obs, T_stat = ma_depth)
+  
+  # return true if we fail to reject
+  return(p_val > alpha)
+}
 
 
 
