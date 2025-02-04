affbb52f673550f63a949bbbd6f8444cae2c50a6
Xinlong Du <xinlongdu@icloud.com>
2025-02-04 11:00:48 -0500

    updated the examples to the function descriptions

--- R/get_CI_function.R
+++ R/get_CI_function.R
@@ -1,5 +1,4 @@
 
-
 #' get_CI
 #'
 #' Given the observed statistic, this function computes a confidence interval given that the data generating process is known using the supplied random seeds.
@@ -14,55 +13,118 @@
 #' @param tol tolerance of the confidence interval.
 #' @param t_init Starting point for the initial search.
 #' @param T_stat A distance metric.
+#' @param print_info Whether or not to print the optim information
 #' @return A vector containing the lower and upper boundary of the confidence interval. In the case when no point is accepted in the search space, return NULL.
 #' @examples
-#' # DP Normal
-#' blahblahblah
+#' ### Regular Normal
+#' set.seed(123)
+#' n <- 100 # sample size
+#' R <- 200 # Repro sample size
+#' alpha <- .05 # significance level
+#' tol <- 1e-4 # tolerance for the confidence set
+#' s_obs <- c(1.12, 0.67) # the observed sample mean and variance
+#' seeds <- matrix(rnorm(R * (n + 2)), nrow = R, ncol = n + 2) # pre-generated seeds
+#'
+#' # this function computes the repro statistics given the seeds and the parameter
+#' s_sample <- function(seeds, theta) {
+#' # generate the raw data points
+#'   raw_data <- theta[1] + sqrt(theta[2]) * seeds[, 1:n]
+#'
+#'   # compute the regular statistics
+#'   s_mean <- apply(raw_data, 1, mean)
+#'   s_var <- apply(raw_data, 1, var)
+#'
+#'   return(cbind(s_mean, s_var))
+#' }
+#'
+#' lower_bds <- c(-5, 0.01) # lower bounds for the parameter search region
+#' upper_bds <- c(5, 5) # upper bounds for the parameter search region
+#'
+#' # choose j = 1 to get the confidence interval for the mean
+#' mean_CI <- get_CI(alpha, lower_bds, upper_bds, 1, seeds, s_sample, s_obs, tol)
+#' print(mean_CI) # estimated confidence interval for mean
+#' var_CI <- get_CI(alpha, lower_bds, upper_bds, 2, seeds, s_sample, s_obs, tol)
+#' print(var_CI) # estimated confidence interval for variance
+#'
+#' ### DP Normal
+#' set.seed(123)
+#' n <- 100 # sample size
+#' R <- 200 # Repro sample size
+#' upper_clamp <- 3
+#' lower_clamp <- 0
+#' eps <- 1 # privacy guarantee parameter
+#' alpha <- .05 # significance level
+#' tol <- 1e-4 # tolerance for the confidence set
+#'
+#' s_obs <- c(1.31, 0.67) # the observed private mean and variance
+#' seeds <-  matrix(rnorm(R * (n + 2)), nrow = R, ncol = n + 2) # pre-generated seeds
+#'
+#' s_sample <- function(seeds, theta) {
+#' # generate the raw data points
+#'   raw_data <- theta[1] + sqrt(theta[2]) * seeds[, 1:n]
+#'
+#'   # clamp the raw data
+#'   clamped <- pmin(pmax(raw_data, lower_clamp), upper_clamp)
+#'
+#'   # compute the private statistics
+#'   s_mean <- apply(clamped, 1, mean) + (upper_clamp - lower_clamp) / (n * eps) * seeds[, n+1]
+#'   s_var <- apply(clamped, 1, var) + (upper_clamp - lower_clamp)^2 / (n * eps) * seeds[, n+2]
+#'
+#'   return(cbind(s_mean, s_var))
+#' }
+#'
+#' dp_mean_CI <- get_CI(alpha, lower_bds, upper_bds, 1, seeds, s_sample, s_obs, tol)
+#' print(dp_mean_CI) # estimated confidence interval for mean
+#' dp_var_CI <- get_CI(alpha, lower_bds, upper_bds, 2, seeds, s_sample, s_obs, tol)
+#' print(dp_var_CI) # estimated confidence interval for variance
+#'
 #' @export
 
 
-get_CI <- function(alpha, lower_bds, upper_bds, j, seeds, G, s_obs, tol, t_init = NULL, T_stat = ma_depth) {
+get_CI <- function(alpha, lower_bds, upper_bds, j, seeds, G, s_obs, tol, t_init = NULL, T_stat = ma_depth, print_info = FALSE) {
   # j indicates that we're computing the confidence interval for the jth parameter
   # tol represents the allowed tolerance on the boundary of our interval
   # T_stat is default to ma_depth as defined in the p_val file
-  
+
   # use the p_value function to identify the best starting point for bisection, if there exits any
-  general_search <- p_value_function:::p_value(lower_bds, upper_bds, seeds, G, s_obs, t_init, T_stat)
-  
+  general_search <- p_value(lower_bds, upper_bds, seeds, G, s_obs, t_init, T_stat)
+
   if (general_search$p_val > alpha) {
     # use the jth coordinate of theta_hat as the starting point for bisection
     beta_init <- general_search$theta_hat[j]
-    
+
     # define the sub_search function which returns whether or not a given interval contains a valid point
     sub_search <- function(beta_left, beta_right, beta_init) {
-      
+
       # print search information for tractability
-      print('current bisection interval')
-      print(c(beta_left, beta_right))
-      print('search starting point')
-      print(beta_init)
-      
+      if (print_info) {
+        print('current bisection interval')
+        print(c(beta_left, beta_right))
+        print('search starting point')
+        print(beta_init)
+      }
+
       updated_lower <- lower_bds
       updated_lower[j] <- beta_left
       updated_upper <- upper_bds
       updated_upper[j] <- beta_right
-      
+
       # call the p_value function on the interval (beta_left, beta_right) with initial point
       initial_point <- (updated_lower + updated_upper) / 2
       initial_point[j] <- beta_init
-      return(p_value_function:::p_value(updated_lower, updated_upper, seeds, G, s_obs, initial_point, T_stat))
+      return(p_value(updated_lower, updated_upper, seeds, G, s_obs, initial_point, T_stat))
     }
-    
+
     # bisection to find the left boundary
     bisect_left <- function(beta_left, beta_right) {
       left <- beta_left
       right <- beta_right
       mid <- (left + right) / 2
-      
+
       while (right-left > tol/2) {
         # use the midpoint as a starting point
         search_result <- sub_search(left, mid, (left + mid) / 2)
-        
+
         if (search_result$p_val > alpha) { # if accepted
           right <- mid
           mid <- (left + right) / 2
@@ -79,17 +141,17 @@ get_CI <- function(alpha, lower_bds, upper_bds, j, seeds, G, s_obs, tol, t_init 
       }
       return(left)
     }
-    
+
     # bisection to find the right boundary
     bisect_right <- function(beta_left, beta_right) {
       left <- beta_left
       right <- beta_right
       mid <- (left + right) / 2
-      
+
       while (right-left > tol/2) {
         # use the midpoint as a starting point
         search_result <- sub_search(mid, right, (mid + right) / 2)
-        
+
         if (search_result$p_val > alpha) {
           left <- mid
           mid <- (left + right) / 2
@@ -106,13 +168,13 @@ get_CI <- function(alpha, lower_bds, upper_bds, j, seeds, G, s_obs, tol, t_init 
       }
       return(right)
     }
-    
+
     # use bisect_left and bisect_right to compute the left/right boundaries
     beta_L <- bisect_left(lower_bds[j], beta_init)
     beta_R <- bisect_right(beta_init, upper_bds[j])
-    
+
     return(c(beta_L, beta_R))
-    
+
   } else {
     return(NULL) # no point in the parameter space is likely, so return the empty set
   }
--- R/p_value_function.R
+++ R/p_value_function.R
@@ -1,68 +1,112 @@
 
-#' p_value
-#'
-#' This is an internal function that uses `depth.Mahalanobis` from the `ddalpha` package.
-#'
-#' @importFrom ddalpha depth.Mahalanobis
 
 # the Mahalanobis depth (modified to also take in a parameter theta)
-# x: each row of x is a vector whose distance we want to compute, 
+# x: each row of x is a vector whose distance we want to compute,
 # data: an (R+1) by d matrix containing (R+1) vectors
 ma_depth <- function(x, data, theta) {
   return(ddalpha::depth.Mahalanobis(x, data))
 }
 
+#' p_value
+#'
+#' Given the observed statistic and the given seeds, this function finds the largest p_value and the corresponding parameter
+#'
+#' @param alpha Significance level.
+#' @param lower_bds Vector containing the lower bounds for the search space.
+#' @param upper_bds Vector containing the upper bounds for the search space
+#' @param seeds Seeds containing all the randomness.
+#' @param G Data generating process.
+#' @param s_obs Observed statistic.
+#' @param tol tolerance of the confidence interval.
+#' @param t_init Starting point for the initial search.
+#' @param T_stat A distance metric.
+#' @param print_info Whether or not to print the optim information
+#' @return This function returns a list containing the minimum p value (p_val) within the search region and the parameter corresponding to it (theta_hat).
+#' @examples
+#' ### Regular Normal
+#' set.seed(123)
+#' n <- 500 # sample size
+#' R <- 200 # Repro sample size
+#' alpha <- .05 # significance level
+#' tol <- 1e-4 # tolerance for the confidence set
+#' s_obs <- c(1.12, 0.67) # the observed sample mean and variance
+#' seeds <- matrix(rnorm(R * (n + 2)), nrow = R, ncol = n + 2) # pre-generated seeds
+#'
+#' # this function computes the repro statistics given the seeds and the parameter
+#' s_sample <- function(seeds, theta) {
+#'   # generate the raw data points
+#'   raw_data <- theta[1] + sqrt(theta[2]) * seeds[, 1:n]
+#'
+#'   # compute the regular statistics
+#'   s_mean <- apply(raw_data, 1, mean)
+#'   s_var <- apply(raw_data, 1, var)
+#'
+#'   return(cbind(s_mean, s_var))
+#' }
+#'
+#' lower_bds <- c(-5, 0.01) # lower bounds for the parameter search region
+#' upper_bds <- c(5, 5) # upper bounds for the parameter search region
+#'
+#' result <- p_value(lower_bds, upper_bds, seeds, s_sample, s_obs)
+#' print(result$p_val) # the largest p_value found
+#' print(result$theta_hat) # the parameter corresponding to the largest p value
+#'
+#'
+#' @importFrom ddalpha depth.Mahalanobis
+#' @export
 
-# p_value function 
-p_value <- function(lower_bds, upper_bds, seeds, G, s_obs, t_init = NULL, T_stat = ma_depth) {
+# p_value function
+p_value <- function(lower_bds, upper_bds, seeds, G, s_obs, t_init = NULL, T_stat = ma_depth, print_info = FALSE) {
   # extract the number of seeds R
   R <- dim(seeds)[1]
   d <- length(s_obs)
-  
+
   # a function that generate R simulated values using the seeds and G, store s_obs and s_sim in an R+1 by d matrix
   s <- function(theta) {
     s_values <- rbind(s_obs, G(seeds, theta))
   }
-  
+
   # function that computes and stores the simulated statistics using T_stat
   statistics <- function(theta) {
     s_matrix <- s(theta)
     t_vec <- T_stat(s_matrix, s_matrix, theta)
-    
+
     return(t_vec)
   }
-  
+
   # define the counting function that we feed into optim
   count <- function(theta) {
     t_values <- statistics(theta)
     ct <- sum(t_values[-1] <= t_values[1]) + t_values[1]
     return(-ct)
   }
-  
+
   # pick the midpoint if t_init is not specified
   if (is.null(t_init)) {
     t_init <- (lower_bds + upper_bds)/2
   }
-  
+
   # call the optim function for minimization
-  opt <- optim(par = t_init, 
+  opt <- optim(par = t_init,
                fn = count,
                method = "L-BFGS-B",
                lower = lower_bds,
                upper = upper_bds)
-  print('opt_result')
-  print(opt)
+  if (print_info) {
+    print("opt result")
+    print(opt)
+  }
   m <- -opt$value
   theta_hat <- opt$par
-  
+
   # compute the p value and return
   p_val <- 1/(R+1) * (min(floor(m), R) + 1)
-  
+
   # compile a list of values to return
   results <- list(p_val = p_val,
                   rank = floor(m)+1,
                   theta_hat = theta_hat)
-  
+
   return(results)
 }
 
